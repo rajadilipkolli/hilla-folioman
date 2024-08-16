d772cdc6494348d9114a0528b8efb7afd7a35874
Raja Kolli <rajadilipkolli@gmail.com>
2024-08-16 21:47:44 +0530

    feat : connect to supabase (#60)
    * feat : connect to supabase
    
    * fixes : format issue
    
    * feat : use supabase for tests
    
    * add back hardcoded password

--- src/test/java/com/example/application/ApplicationTest.java
+++ src/test/java/com/example/application/ApplicationTest.java
@@ -1,13 +1,11 @@
 package com.example.application;
 
-import com.example.application.common.SQLContainersConfig;
+import com.example.application.common.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.modulith.core.ApplicationModules;
 import org.springframework.modulith.docs.Documenter;
 
-@SpringBootTest(classes = SQLContainersConfig.class)
-class ApplicationTest {
+class ApplicationTest extends AbstractIntegrationTest {
 
     @Test
     void contextLoads() {
