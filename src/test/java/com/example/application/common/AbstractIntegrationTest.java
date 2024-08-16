f85f85d758d920a36f88310baf5ff72aadd99f0c
Raja Kolli <rajadilipkolli@gmail.com>
2024-08-16 07:06:18 +0000

    feat : connect to supabase

---
+++ src/test/java/com/example/application/common/AbstractIntegrationTest.java
@@ -0,0 +1,25 @@
+package com.example.application.common;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ActiveProfiles({"supabase"})
+@SpringBootTest(
+        webEnvironment = RANDOM_PORT,
+        classes = {SQLContainersConfig.class})
+@AutoConfigureMockMvc
+public abstract class AbstractIntegrationTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+}
+
