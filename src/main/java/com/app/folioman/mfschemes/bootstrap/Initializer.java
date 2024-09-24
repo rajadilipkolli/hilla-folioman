f125559585f28868118587cfb82a3d3eebfb011b
Raja Kolli <rajadilipkolli@gmail.com>
2024-09-24 04:27:17 +0000

    optimize memory management

--- src/main/java/com/app/folioman/mfschemes/bootstrap/Initializer.java
+++ src/main/java/com/app/folioman/mfschemes/bootstrap/Initializer.java
@@ -66,7 +66,7 @@ public class Initializer {
                         bseStarMasterDataService.fetchBseStarMasterData(amfiDataMap, amfiCodeIsinMapping);
 
                 // Process data
-                processMasterData(bseStarMasterDataMap, amfiDataMap);
+                processMasterData(bseStarMasterDataMap, amfiDataMap.keySet());
                 LOGGER.debug("Completed loading initial data.");
             }
         } catch (HttpClientErrorException | IOException httpClientErrorException) {
@@ -104,12 +104,11 @@ public class Initializer {
     }
 
     // Parallel processing for better performance
-    private void processMasterData(
-            Map<String, MfFundScheme> bseStarMasterDataMap, Map<String, Map<String, String>> amfiDataMap) {
+    private void processMasterData(Map<String, MfFundScheme> bseStarMasterDataMap, Set<String> amfiCodeSet) {
 
         Set<String> distinctAmfiCodeFromDB = new HashSet<>(this.mfFundSchemeService.findDistinctAmfiCode());
         List<MfFundScheme> mfFundSchemeList = bseStarMasterDataMap.keySet().stream()
-                .filter(amfiDataMap::containsKey)
+                .filter(amfiCodeSet::contains)
                 .filter(s -> !distinctAmfiCodeFromDB.contains(s))
                 .distinct()
                 .map(bseStarMasterDataMap::get)
