45a86f4728cfbd6a9f720ba701d9418eb748956d
Raja Kolli <rajadilipkolli@gmail.com>
2024-10-03 17:58:43 +0000

    feat : adds validation & tests

--- src/main/java/com/app/folioman/portfolio/web/controller/ImportMutualFundController.java
+++ src/main/java/com/app/folioman/portfolio/web/controller/ImportMutualFundController.java
@@ -5,18 +5,26 @@ import com.app.folioman.portfolio.models.response.UploadFileResponse;
 import com.app.folioman.portfolio.service.UserDetailService;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.hilla.Endpoint;
+import jakarta.validation.constraints.PastOrPresent;
 import java.io.IOException;
 import java.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @AnonymousAllowed
 @Endpoint
 @RestController
+@Validated
 public class ImportMutualFundController {
 
     private static final Logger log = LoggerFactory.getLogger(ImportMutualFundController.class);
@@ -36,7 +44,10 @@ public class ImportMutualFundController {
     @GetMapping("/api/portfolio/{pan}")
     public PortfolioResponse getPortfolio(
             @PathVariable("pan") String panNumber,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate asOfDate) {
+            @RequestParam(required = false)
+                    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                    @PastOrPresent(message = "Date should be past or today")
+                    LocalDate asOfDate) {
         return userDetailService.getPortfolioByPAN(panNumber, asOfDate);
     }
 }
