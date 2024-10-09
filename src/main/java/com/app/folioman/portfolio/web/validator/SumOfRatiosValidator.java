12865958bd5a06c922afc6dd9bce94783966e994
Raja Kolli <rajadilipkolli@gmail.com>
2024-10-10 00:34:53 +0530

    feat : adds rebalance calculator (#246)
    * feat : adds rebalance calculator
    
    * adds validation
    
    * adds more validation
    
    * typo fix
    
    * polish testcases
    
    * improve screen reader
    
    * feat : adds more validations

---
+++ src/main/java/com/app/folioman/portfolio/web/validator/SumOfRatiosValidator.java
@@ -0,0 +1,30 @@
+package com.app.folioman.portfolio.web.validator;
+
+import com.app.folioman.portfolio.models.request.Fund;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.util.List;
+
+public class SumOfRatiosValidator implements ConstraintValidator<ValidSumOfRatios, List<Fund>> {
+
+    @Override
+    public boolean isValid(List<Fund> funds, ConstraintValidatorContext context) {
+        if (funds == null || funds.isEmpty()) {
+            return true;
+        }
+
+        double totalRatio = funds.stream().mapToDouble(Fund::ratio).sum();
+
+        // Check if the sum of ratios is approximately 1
+        boolean isValid = Math.abs(totalRatio - 1.00) < 0.001; // Allowing a small margin for floating-point errors
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(
+                            "The sum of fund ratios must be 1%. Current sum: " + String.format("%.2f", totalRatio))
+                    .addConstraintViolation();
+        }
+
+        return isValid;
+    }
+}
---
+++ src/test/java/com/app/folioman/portfolio/web/controller/ReBalanceControllerTest.java
@@ -0,0 +1,179 @@
+package com.app.folioman.portfolio.web.controller;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.app.folioman.portfolio.models.request.Fund;
+import com.app.folioman.portfolio.models.request.InvestmentRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = ReBalanceController.class)
+@AutoConfigureMockMvc
+class ReBalanceControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void testReBalance() throws Exception {
+        this.mockMvc
+                .perform(post("/api/portfolio/rebalance")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new InvestmentRequest(
+                                List.of(new Fund(7000, 0.7), new Fund(3000, 0.25), new Fund(500, 0.05)), 1000)))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.investments.size()", is(3)));
+    }
+
+    @Test
+    void testReBalanceWithNullInvestmentRequest() throws Exception {
+        mockMvc.perform(post("/api/portfolio/rebalance").content("{}").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE))
+                .andExpect(jsonPath("$.type", is("about:blank")))
+                .andExpect(jsonPath("$.title", is("Constraint Violation")))
+                .andExpect(jsonPath("$.status", is(400)))
+                .andExpect(jsonPath("$.detail", is("Invalid request content.")))
+                .andExpect(jsonPath("$.instance", is("/api/portfolio/rebalance")))
+                .andExpect(jsonPath("$.violations", hasSize(2)))
+                .andExpect(jsonPath("$.violations[0].field", is("amountToInvest")))
+                .andExpect(jsonPath("$.violations[0].message", is("Amount to invest cannot be negative")))
+                .andExpect(jsonPath("$.violations[1].field", is("funds")))
+                .andExpect(jsonPath(
+                        "$.violations[1].message", is("Investment request and funds list cannot be null or empty")));
+    }
+
+    @Test
+    void testReBalanceWithEmptyFundsList() throws Exception {
+        InvestmentRequest investmentRequest = new InvestmentRequest(
+                List.of(), // Empty funds list
+                1000);
+
+        mockMvc.perform(post("/api/portfolio/rebalance")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(investmentRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE))
+                .andExpect(jsonPath("$.type", is("about:blank")))
+                .andExpect(jsonPath("$.title", is("Constraint Violation")))
+                .andExpect(jsonPath("$.status", is(400)))
+                .andExpect(jsonPath("$.detail", is("Invalid request content.")))
+                .andExpect(jsonPath("$.instance", is("/api/portfolio/rebalance")))
+                .andExpect(jsonPath("$.violations", hasSize(1)))
+                .andExpect(jsonPath("$.violations[0].field", is("funds")))
+                .andExpect(jsonPath(
+                        "$.violations[0].message", is("Investment request and funds list cannot be null or empty")));
+    }
+
+    @Test
+    void testReBalanceWithNegativeAmountToInvest() throws Exception {
+        InvestmentRequest investmentRequest =
+                new InvestmentRequest(List.of(new Fund(10000, 0.5)), -1000); // Negative amount to invest
+
+        mockMvc.perform(post("/api/portfolio/rebalance")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(investmentRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE))
+                .andExpect(jsonPath("$.type", is("about:blank")))
+                .andExpect(jsonPath("$.title", is("Constraint Violation")))
+                .andExpect(jsonPath("$.status", is(400)))
+                .andExpect(jsonPath("$.detail", is("Invalid request content.")))
+                .andExpect(jsonPath("$.instance", is("/api/portfolio/rebalance")))
+                .andExpect(jsonPath("$.violations", hasSize(2)))
+                .andExpect(jsonPath("$.violations[0].field", is("amountToInvest")))
+                .andExpect(jsonPath("$.violations[0].message", is("Amount to invest cannot be negative")))
+                .andExpect(jsonPath("$.violations[1].field", is("funds")))
+                .andExpect(jsonPath(
+                        "$.violations[1].message", is("The sum of fund ratios must be 1%. Current sum: 0.50")));
+    }
+
+    @Test
+    void testReBalanceWithInvalidFundRatios() throws Exception {
+        InvestmentRequest investmentRequest = new InvestmentRequest(
+                List.of(
+                        new Fund(10000, 0.50), new Fund(10000, 0.30)
+                        // Sum of ratios is 0.80, not 1.00
+                        ),
+                1000);
+
+        mockMvc.perform(post("/api/portfolio/rebalance")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(investmentRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE))
+                .andExpect(jsonPath("$.type", is("about:blank")))
+                .andExpect(jsonPath("$.title", is("Constraint Violation")))
+                .andExpect(jsonPath("$.status", is(400)))
+                .andExpect(jsonPath("$.detail", is("Invalid request content.")))
+                .andExpect(jsonPath("$.instance", is("/api/portfolio/rebalance")))
+                .andExpect(jsonPath("$.violations", hasSize(1)))
+                .andExpect(jsonPath("$.violations[0].field", is("funds")))
+                .andExpect(jsonPath(
+                        "$.violations[0].message", is("The sum of fund ratios must be 1%. Current sum: 0.80")));
+    }
+
+    @Test
+    void testReBalanceWithNegativeFundValue() throws Exception {
+        InvestmentRequest investmentRequest = new InvestmentRequest(
+                List.of(new Fund(-10000, 0.5)), // Negative fund value
+                1000);
+
+        mockMvc.perform(post("/api/portfolio/rebalance")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(investmentRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE))
+                .andExpect(jsonPath("$.type", is("about:blank")))
+                .andExpect(jsonPath("$.title", is("Constraint Violation")))
+                .andExpect(jsonPath("$.status", is(400)))
+                .andExpect(jsonPath("$.detail", is("Invalid request content.")))
+                .andExpect(jsonPath("$.instance", is("/api/portfolio/rebalance")))
+                .andExpect(jsonPath("$.violations", hasSize(2)))
+                .andExpect(jsonPath("$.violations[0].field", is("funds")))
+                .andExpect(
+                        jsonPath("$.violations[0].message", is("The sum of fund ratios must be 1%. Current sum: 0.50")))
+                .andExpect(jsonPath("$.violations[1].field", is("funds[0].value")))
+                .andExpect(jsonPath("$.violations[1].message", is("Fund value cannot be negative")));
+    }
+
+    @Test
+    void testReBalanceWithInvalidFundRatio() throws Exception {
+        InvestmentRequest investmentRequest = new InvestmentRequest(
+                List.of(new Fund(10000, 1.50)), // Ratio greater than 100
+                1000);
+
+        mockMvc.perform(post("/api/portfolio/rebalance")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(investmentRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE))
+                .andExpect(jsonPath("$.type", is("about:blank")))
+                .andExpect(jsonPath("$.title", is("Constraint Violation")))
+                .andExpect(jsonPath("$.status", is(400)))
+                .andExpect(jsonPath("$.detail", is("Invalid request content.")))
+                .andExpect(jsonPath("$.instance", is("/api/portfolio/rebalance")))
+                .andExpect(jsonPath("$.violations", hasSize(2)))
+                .andExpect(jsonPath("$.violations[0].field", is("funds")))
+                .andExpect(
+                        jsonPath("$.violations[0].message", is("The sum of fund ratios must be 1%. Current sum: 1.50")))
+                .andExpect(jsonPath("$.violations[1].field", is("funds[0].ratio")))
+                .andExpect(jsonPath("$.violations[1].message", is("Fund ratio must be between 0 and 1")));
+    }
+}
