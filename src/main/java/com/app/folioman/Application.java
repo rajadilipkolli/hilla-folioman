882f455f5109ea7056ed7112c2381f91d449698d
Raja Kolli <rajadilipkolli@gmail.com>
2024-10-27 08:35:03 +0000

    fix : final violation

--- src/main/java/com/app/folioman/Application.java
+++ src/main/java/com/app/folioman/Application.java
@@ -1,11 +1,9 @@
 package com.app.folioman;
 
-import com.app.folioman.mfschemes.config.ApplicationProperties;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 /**
  * The entry point of the Spring Boot application.
@@ -16,7 +14,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
  */
 @SpringBootApplication
 @Theme(value = "hilla-folioman")
-@ConfigurationPropertiesScan(basePackageClasses = ApplicationProperties.class)
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
