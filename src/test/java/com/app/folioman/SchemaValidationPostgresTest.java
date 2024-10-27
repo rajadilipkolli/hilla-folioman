238f660a1412982f74b4fb2b6796ebb92fc77e25
Raja Kolli <rajadilipkolli@gmail.com>
2024-10-27 05:51:53 +0000

    attempt to fix

--- src/test/java/com/app/folioman/SchemaValidationPostgresTest.java
+++ src/test/java/com/app/folioman/SchemaValidationPostgresTest.java
@@ -12,11 +12,13 @@ import java.util.Set;
 import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
-@DataJpaTest(properties = {"spring.jpa.hibernate.ddl-auto=validate", "spring.test.database.replace=none"})
+@DataJpaTest(properties = {"spring.jpa.hibernate.ddl-auto=validate"})
 @Import(SQLContainersConfig.class)
+@AutoConfigureTestDatabase
 class SchemaValidationPostgresTest {
 
     @Autowired
