94866e5eca113133eb2e458d4c2a64f9b48ee94b
Raja Kolli <rajadilipkolli@gmail.com>
2024-10-31 15:17:37 +0000

    adds missing configuration, documentation

--- src/main/java/com/app/folioman/config/LazyConnectionDataSourceProxyConfig.java
+++ src/main/java/com/app/folioman/config/LazyConnectionDataSourceProxyConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 import org.springframework.lang.NonNull;
 
@@ -16,12 +17,24 @@ import org.springframework.lang.NonNull;
  * particularly useful in scenarios where database operations don't always occur during request
  * processing.
  */
+@Configuration(proxyBeanMethods = false)
 class LazyConnectionDataSourceProxyConfig implements BeanFactoryPostProcessor {
 
+    /**
+     * Processes the bean factory after its standard initialization.
+     * This method:
+     * 1. Registers a new LazyConnectionDataSourceProxy as the primary datasource
+     * 2. Configures the original datasource for JobRunr usage
+     *
+     * @throws BeansException if the "dataSource" bean is not found
+     */
     @Override
     public void postProcessBeanFactory(@NonNull ConfigurableListableBeanFactory beanFactory) throws BeansException {
         DefaultListableBeanFactory listableBeanFactory = (DefaultListableBeanFactory) beanFactory;
 
+        if (!listableBeanFactory.containsBean("dataSource")) {
+            throw new BeansException("Required 'dataSource' bean is not defined") {};
+        }
         // Register LazyConnectionDataSourceProxy as a primary bean
         listableBeanFactory.registerBeanDefinition(
                 "lazyConnectionDataSourceProxy", createLazyConnectionDataSourceProxyDefinition(listableBeanFactory));
