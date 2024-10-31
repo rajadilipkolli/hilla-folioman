89e0a9bb5930211ddb5571d44b01f6a71d88a40c
Raja Kolli <rajadilipkolli@gmail.com>
2024-10-31 14:49:15 +0000

    feat : using BeanFactoryPostProcessor to set bean

--- src/main/java/com/app/folioman/config/LazyConnectionDataSourceProxyConfig.java
+++ src/main/java/com/app/folioman/config/LazyConnectionDataSourceProxyConfig.java
@@ -1,13 +1,14 @@
 package com.app.folioman.config;
 
 import javax.sql.DataSource;
-import net.ttddyy.observation.boot.autoconfigure.DataSourceObservationAutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import org.springframework.lang.NonNull;
 
 /**
  * Configuration class that sets up a lazy-loading data source proxy to improve performance by
@@ -15,14 +16,29 @@ import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
  * particularly useful in scenarios where database operations don't always occur during request
  * processing.
  */
-@AutoConfiguration(
-        after = {DataSourceAutoConfiguration.class},
-        before = {DataSourceObservationAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-class LazyConnectionDataSourceProxyConfig {
+class LazyConnectionDataSourceProxyConfig implements BeanFactoryPostProcessor {
 
-    @Bean
-    @Primary
-    DataSource lazyConnectionDataSourceProxy(DataSource dataSource) {
-        return new LazyConnectionDataSourceProxy(dataSource);
+    @Override
+    public void postProcessBeanFactory(@NonNull ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        DefaultListableBeanFactory listableBeanFactory = (DefaultListableBeanFactory) beanFactory;
+
+        // Register LazyConnectionDataSourceProxy as a primary bean
+        listableBeanFactory.registerBeanDefinition(
+                "lazyConnectionDataSourceProxy", createLazyConnectionDataSourceProxyDefinition(listableBeanFactory));
+
+        // Update existing dataSource bean definition
+        BeanDefinition beanDefinition = beanFactory.getBeanDefinition("dataSource");
+        beanDefinition.getPropertyValues().add("poolName", "jobrunr");
+        listableBeanFactory.registerBeanDefinition("jobrunrDataSource", beanDefinition);
+    }
+
+    private GenericBeanDefinition createLazyConnectionDataSourceProxyDefinition(
+            DefaultListableBeanFactory listableBeanFactory) {
+        GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+        beanDefinition.setBeanClass(LazyConnectionDataSourceProxy.class);
+        beanDefinition.setInstanceSupplier(
+                () -> new LazyConnectionDataSourceProxy(listableBeanFactory.getBean("dataSource", DataSource.class)));
+        beanDefinition.setPrimary(true); // Set this bean as primary
+        return beanDefinition;
     }
 }
