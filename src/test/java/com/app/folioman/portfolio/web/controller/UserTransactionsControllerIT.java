d82ec7abaf19a3270fe97c1866b9dec641099fce
Raja Kolli <rajadilipkolli@gmail.com>
2024-11-05 11:18:54 +0530

    feat: adds investments endpoint IT (#347)
    * feat: adds investments endpoint IT
    
    * spotless fix
    
    * adds negative test cases
    
    * fix : assertions

---
+++ src/test/java/com/app/folioman/portfolio/web/controller/UserTransactionsControllerIT.java
@@ -0,0 +1,29 @@
+package com.app.folioman.portfolio.web.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.app.folioman.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+class UserTransactionsControllerIT extends AbstractIntegrationTest {
+
+    @Test
+    void getTotalInvestmentsByPanPerMonth() throws Exception {
+        this.mockMvc
+                .perform(get("/api/portfolio/investments/{pan}", "ABCDE1234F").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray());
+    }
+
+    @Test
+    void getTotalInvestmentsByPanPerMonth_WithInvalidPan_ShouldReturnBadRequest() throws Exception {
+        this.mockMvc
+                .perform(get("/api/portfolio/investments/{pan}", "INVALID-PAN").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+}
