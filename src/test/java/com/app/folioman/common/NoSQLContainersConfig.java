29bbb44c3aa7f4e3f3683d0e4840fba46a9a2277
Raja Kolli <rajadilipkolli@gmail.com>
2024-11-16 14:57:58 +0000

    enable lgtm

--- src/test/java/com/app/folioman/common/NoSQLContainersConfig.java
+++ src/test/java/com/app/folioman/common/NoSQLContainersConfig.java
@@ -1,10 +1,12 @@
 package com.app.folioman.common;
 
 import com.redis.testcontainers.RedisStackContainer;
+import java.time.Duration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.grafana.LgtmStackContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -21,5 +23,12 @@ public class NoSQLContainersConfig {
     @ServiceConnection
     MongoDBContainer mongoDBContainer() {
         return new MongoDBContainer(DockerImageName.parse("mongo").withTag("8.0.3"));
+    }
+
+    @Bean
+    @ServiceConnection
+    LgtmStackContainer lgtmContainer() {
+        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:0.7.8"))
+                .withStartupTimeout(Duration.ofMinutes(2));
     }
 }
