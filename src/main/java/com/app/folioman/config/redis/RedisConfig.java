a7072c5f036a45f12b32d64a7fb0927aee0f3774
Raja Kolli <rajadilipkolli@gmail.com>
2024-12-18 10:11:22 +0000

    feat : handle exception in cache

--- src/main/java/com/app/folioman/config/redis/RedisConfig.java
+++ src/main/java/com/app/folioman/config/redis/RedisConfig.java
@@ -1,6 +1,9 @@
 package com.app.folioman.config.redis;
 
+import org.springframework.cache.Cache;
+import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheWriter;
@@ -8,10 +11,11 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.lang.Nullable;
 
 @Configuration(proxyBeanMethods = false)
 @EnableCaching
-class RedisConfig {
+class RedisConfig implements CachingConfigurer {
 
     @Bean
     RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
@@ -26,5 +30,33 @@ class RedisConfig {
     CustomRedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory, Monitor monitor) {
         RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory);
         return new CustomRedisCacheManager(redisCacheWriter, redisConnectionFactory, monitor);
+    }
+
+    @Override
+    public CacheErrorHandler errorHandler() {
+
+        return new CacheErrorHandler() {
+
+            @Override
+            public void handleCacheGetError(RuntimeException exception, Cache cache, Object key) {
+                // your custom error handling logic
+            }
+
+            @Override
+            public void handleCachePutError(
+                    RuntimeException exception, Cache cache, Object key, @Nullable Object value) {
+                // your custom error handling logic
+            }
+
+            @Override
+            public void handleCacheEvictError(RuntimeException exception, Cache cache, Object key) {
+                // your custom error handling logic
+            }
+
+            @Override
+            public void handleCacheClearError(RuntimeException exception, Cache cache) {
+                // your custom error handling logic
+            }
+        };
     }
 }
