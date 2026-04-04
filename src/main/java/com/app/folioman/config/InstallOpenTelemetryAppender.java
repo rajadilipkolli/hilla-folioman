e7016def26743a88cc359fe7d25959739dc8bb68
Raja Kolli <rajadilipkolli@gmail.com>
2026-04-04 12:56:04 +0000

    fix as per code review

--- src/main/java/com/app/folioman/config/InstallOpenTelemetryAppender.java
+++ src/main/java/com/app/folioman/config/InstallOpenTelemetryAppender.java
@@ -3,8 +3,6 @@ package com.app.folioman.config;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.task.support.ContextPropagatingTaskDecorator;
 import org.springframework.stereotype.Component;
 
 // Install OpenTelemetry Appender
@@ -20,12 +18,5 @@ class InstallOpenTelemetryAppender implements InitializingBean {
     @Override
     public void afterPropertiesSet() {
         OpenTelemetryAppender.install(this.openTelemetry);
-    }
-
-    // When using @Async methods or AsyncTaskExecutor, context gets lost in new threads. Fix this by configuring the
-    // ContextPropagatingTaskDecorator
-    @Bean
-    ContextPropagatingTaskDecorator contextPropagatingTaskDecorator() {
-        return new ContextPropagatingTaskDecorator();
     }
 }
