989b896d50a93e3a5e931e85ff66a1b1a9dc4a89
Raja Kolli <rajadilipkolli@gmail.com>
2026-05-22 22:56:33 +0000

    fix : issue with parsing JPQL

--- src/main/java/com/app/folioman/auth/domain/UserRepository.java
+++ src/main/java/com/app/folioman/auth/domain/UserRepository.java
@@ -4,6 +4,8 @@ import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,5 +14,6 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsername(String username);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    Optional<UserEntity> findByUsernameForUpdate(String username);
+    @Query("select u from UserEntity u where u.username = :username")
+    Optional<UserEntity> findByUsernameForUpdate(@Param("username") String username);
 }
