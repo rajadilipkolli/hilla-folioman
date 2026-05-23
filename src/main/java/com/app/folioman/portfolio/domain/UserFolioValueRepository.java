d80615a531e62b1bffdf3e5014d136693d18e9b6
Raja Kolli <rajadilipkolli@gmail.com>
2026-05-23 12:14:43 +0000

    fix : arch unit violation

--- src/main/java/com/app/folioman/portfolio/domain/UserFolioValueRepository.java
+++ src/main/java/com/app/folioman/portfolio/domain/UserFolioValueRepository.java
@@ -4,7 +4,9 @@ import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 interface UserFolioValueRepository extends JpaRepository<UserFolioValueEntity, Long> {
 
     List<UserFolioValueEntity> findByUserFolioDetailsEntity_IdInAndDateBetween(
