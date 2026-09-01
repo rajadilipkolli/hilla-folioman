ea36f6b5f9aebbffc0c62b7cd7a4df931480b91b
Raja Kolli <rajadilipkolli@gmail.com>
2026-09-01 07:22:39 +0530

    Fix : test case failures

--- src/test/java/com/app/folioman/config/NoSQLContainersConfig.java
+++ src/test/java/com/app/folioman/config/NoSQLContainersConfig.java
@@ -41,7 +41,7 @@ public class NoSQLContainersConfig {
     @Bean
     @ServiceConnection
     LgtmStackContainer lgtmContainer() {
-        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:0.31.0"))
+        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:0.32.0"))
                 .withStartupTimeout(Duration.ofMinutes(2));
     }
 }
